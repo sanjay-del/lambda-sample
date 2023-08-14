8f3b90bc7cf2802fb08dc305da47c336e463889c
sanjay-del <sanjaypantha.np@gmail.com>
2023-08-14 15:51:49 +0545

    event  emit test in lambda

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,28 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const AWS= require('aws-sdk')
+AWS.config.update({region: 'us-west-2'});
 const validateDid= async(req,res)=>{
   const did = req.query.did
   console.log(did)
+  const eventbridge = new AWS.EventBridge({eventBusName: 'event-relay-bus'});
+  let response;  
+  // Define the event details
+  const eventDetails = {
+      Source: 'eventProducerApp',
+      DetailType: 'inbound-event-sent',
+      Detail: JSON.stringify({ "event-id": "123", "return-response-event": true })
+  };
+
   const resa = await fetch(`https://resolver.itn.mobi/1.0/identifiers/${did}`)
   if(resa.status===200){
-   return res.status(200).send('Itn membership found')
+    try {
+      response = await eventbridge.putEvents({ Entries: [eventDetails] }).promise();
+      console.log('Event sent to EventBridge:', response);
+    } catch (error) {
+      console.error('Error sending event:', error);
+    }
+   return res.status(200).send(`Itn membership found \
+   ${response}`)
   }
   else{
     return res.status(404).send('Itn membership not found')
